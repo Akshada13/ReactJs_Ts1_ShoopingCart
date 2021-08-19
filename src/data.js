3e36308512f956f8ea15f985ffb271514d7d88a9
Akshada Shelke <84895276+Akshada13@users.noreply.github.com>
2021-08-19 16:54:17 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data =  {
+    products: 
+    [
+        {
+            id: '1',
+            name: 'MacBook',
+            price: 1400,
+            image: 'https://picsum.photos/id/180/2400/1600',
+       },
+       {
+           id: '2',
+           name: 'Car',
+           price: 2500,
+           image: 'https://picsum.photos/id/1071/3000/1996',
+       },
+       {
+           id: '3',
+           name: 'Camera',
+           price: 1500,
+           image: 'https://picsum.photos/id/454/4403/2476',
+       },
+    ],
+};
+export default data;
